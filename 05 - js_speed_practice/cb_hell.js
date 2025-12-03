a29002761ad6a0de858066fda99442e58d3cb79d
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-03 13:03:59 +0530

    refactor: improve comments for clarity on callback hell example and error handling

--- 05 - js_speed_practice/cb_hell.js
+++ 05 - js_speed_practice/cb_hell.js
@@ -1,18 +1,35 @@
 import fs from "fs";
 
-// callback hell
+// -------------------------------
+// CALLBACK HELL:
+// - each operation depends on the previous one
+// - to resolve callback hell we can use promises, async/await
+// -------------------------------
+
+// -------------------------------
+// EXAMPLE:
+// - Read contents of "hello.txt"
+// - Create "backup.txt"
+// - Write contents of "hello.txt" into "backup.txt"
+// - Delete "hello.txt"
+// -------------------------------
+
 function cbHell() {
-  fs.readFile("./hello.txt", "utf8", (err, data) => {
+  fs.readFile("./hello.txt", "utf8", (err, fileContents) => {
     if (err) {
-      console.log(err);
+      console.log("Error reading hello.txt file: " + err);
     } else {
-      fs.writeFile("./backup.txt", data, (err) => {
+      console.log("Successfully read hello.txt file");
+      fs.writeFile("./backup.txt", fileContents, (err) => {
         if (err) {
-          console.log(err);
+          console.log('Error writing backup.txt file: ' + err);
         } else {
+          console.log('Successfully wrote backup.txt file');
           fs.unlink("./hello.txt", (err) => {
             if (err) {
-              console.log(err);
+              console.log('Error deleting hello.txt file: ' + err);
+            } else {
+              console.log('Successfully deleted hello.txt file');
             }
           });
         }
@@ -22,5 +39,3 @@ function cbHell() {
 }
 
 cbHell();
-
-// to resolve callback hell we can use promises
