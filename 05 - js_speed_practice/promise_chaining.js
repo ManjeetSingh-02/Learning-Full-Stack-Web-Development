15f44672ef09d2a135de36aac3571ce9c3317a22
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-03 13:11:26 +0530

    refactor: enhance comments for clarity on promise chaining and provide a detailed example

--- 05 - js_speed_practice/promise_chaining.js
+++ 05 - js_speed_practice/promise_chaining.js
@@ -1,15 +1,25 @@
 import fs from "fs/promises";
 
-// promise chaining
+// --------------------------------------
+// PROMISE CHAINING
+// - in this we chain multiple .then() calls till the final result is achieved
+// - each .then() receives the result of the previous .then()
+// - if any promise in the chain is rejected, the control jumps to the nearest .catch()
+// - .finally() is executed at the end regardless of the outcome
+// This multiple .then() chaining can lead to "callback hell" like structure known as "Promise Hell"
+// To resolve the PROMISE HELL, we can use async/await
+// --------------------------------------
+
+// -------------------------------
+// EXAMPLE:
+// - Read contents of "hello.txt"
+// - Create "backup.txt"
+// - Write contents of "hello.txt" into "backup.txt"
+// - Delete "hello.txt"
+// -------------------------------
 fs.readFile("./hello.txt", "utf8")
   .then((data) => fs.writeFile("./backup.txt", data))
   .then(() => fs.unlink("./hello.txt"))
-  .then(() => console.log("done1"))
-  .then(() => console.log("done2"))
-  .then(() => console.log("done3"))
-  .then(() => console.log("done4"))
-  .then(() => console.log("done5"))
   .catch((err) => console.log(err))
   .finally(() => console.log("finally"));
 
-// to resolve the promise hell, we can use async await
