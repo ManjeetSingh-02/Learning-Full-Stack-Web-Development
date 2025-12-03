d8a2a1b5daee69c9e5f613b4f8cb1689795977c2
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-03 21:10:11 +0530

    refactor: enhance comments for clarity on debouncing and throttling techniques with detailed examples

--- 05 - js_speed_practice/mac_code_debouncing.js
+++ 05 - js_speed_practice/mac_code_debouncing.js
@@ -1,5 +1,17 @@
-// function(parent) returning a function(child) is called a higher order function
-// this concept is called closure where returned function has access to the variables of the parent function
+//----------------------------------------
+// DEBOUNCING: a technique to delay the execution of a function until after a certain amount of time has passed since it was last invoked
+// if a user is clicking the login button multiple times, this concept ensures that the last login request is the only one that gets processed after a certain delay
+// if a delay is active and the user clicks again, the previous delay is cleared and a new delay starts
+//----------------------------------------
+
+//----------------------------------------
+// HIGHER ORDER FUNCTION: a function that takes another function as an argument or returns a child function as its result
+//----------------------------------------
+
+//----------------------------------------
+// CLOSURE: when a child function that is returned from a parent function and the child function has access to the variables of the parent function even after the parent function has finished executing
+//----------------------------------------
+
 
 function f1(fn, delay) {
   let myId = null;
@@ -9,13 +21,21 @@ function f1(fn, delay) {
   };
 }
 
+// Execution of f1 function:
+// 1. f1 has 2 parameters: fn (a function) and delay (a number in milliseconds).
+// 2. Inside f1, a variable myId is initialized to null. This variable will hold the ID of the setTimeout fn.
+// 3. f1 returns an inner function (child function) that takes any number of arguments (...args).
+// 4. child function clears any previously setTimeout calls using clearTimeout(myId).
+// 5. Then it sets a new timeout using setTimeout, which will call the fn function after the specified delay
+// 6. Due to apply(this, args), the new fn have access to the arguments and context of the child function when it is eventually called after the delay.
+
 function greet(msg) {
   console.log(msg);
 }
 
 const x = f1((msg) => greet(msg), 3000);
 
-x(1);
-x(2);
-x(3);
-x(4);
+x("Hi 1");
+x("Hi 2");
+x("Hi 3");
+// only "Hi 3" will be printed after 3 seconds
--- 05 - js_speed_practice/mac_code_throttling.js
+++ 05 - js_speed_practice/mac_code_throttling.js
@@ -1,3 +1,9 @@
+// -------------------------------------
+// THROTTLING: a technique which ensures that a function is only called once within a specified time frame
+// if a user is clicking the login button multiple times, this concept ensures that the first login request is the only one that gets processed in that time frame
+// if the user requests again within a specified time frame (delay), those subsequent requests are ignored
+// -------------------------------------
+
 const f1 = (fn, delay) => {
   let myId = null;
 
@@ -9,7 +15,19 @@ const f1 = (fn, delay) => {
   };
 };
 
-const x = f1(() => console.log("hi"), 3000);
-x();
-x();
-x();
+// Execution of f1 function:
+// 1. f1 has 2 parameters: fn (a function) and delay (a number in milliseconds).
+// 2. Inside f1, a variable myId is initialized to null. This variable will hold the ID of the setTimeout fn.
+// 3. f1 returns an inner function (child) that takes any number of arguments (...args)
+// 4. child function checks if myId is null
+// 5. If myId is null, it calls the fn function with the provided arguments (...args) and sets myId to a new setTimeout fn
+// 6. New setTimeout fn will reset myId to null after the specified delay time
+// 7. During the delay time, any subsequent requests to the child function will be ignored because myId is not null
+// 7. After the delay time has passed, myId is reset to null, allowing future requests
+// 8. This ensures that fn is only called once within the specified delay time frame
+
+const x = f1(msg => console.log(msg), 3000);
+x("Hello 1");
+x("Hello 2");
+x("Hello 3");
+// only "Hello 1" will be printed immediately, "Hello 2" and "Hello 3" will be ignored
