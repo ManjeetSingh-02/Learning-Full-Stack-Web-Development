c52e3c8bb2a18df964fe6f6829020d1287f34755
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-03 12:21:28 +0530

    fix: clarify comments on proxy behavior and object copying in mac_code_negativeIndex.js

--- 05 - js_speed_practice/mac_code_negativeIndex.js
+++ 05 - js_speed_practice/mac_code_negativeIndex.js
@@ -1,10 +1,10 @@
 // PROXY - this creates a proxy for an object, allowing to define custom behavior and operations on the object
-// the proxied object always points to the original object as reference is same as the original object
-// to prevent the change in original object, we can create a copy of the original object (it can be shallow or deep copy based on the requirement)
+// the proxied object always points to the address of the original object, so any changes made to the proxied object will also reflect in the original object
 
 // a proxy to handle negative indexing in arrays
 function proxiedArr(arr) {
   return new Proxy(arr, {
+    // target -> it holds the address of the original object
     get(target, index) {
       index = Number(index);
       if (index >= 0) return target[index];
@@ -29,3 +29,9 @@ console.log(newArr[-1]);
 // arr and newArr point to same reference, so any changes that are made by proxy will also reflect in arr
 newArr[-1] = 5;
 console.log(newArr[-1]);
+
+// to prevent the change in original object, we can create a copy of the original object
+// copy can be of 2 types:
+// shallow copy - [...obj]
+// deep copy - JSON.parse(JSON.stringify(obj))
+let copyArr = proxiedArr([...arr]);
